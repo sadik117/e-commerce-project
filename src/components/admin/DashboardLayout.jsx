b35b7bcffd65dc05712f049711cf25d7c8e78954
unknown <sadiksourov11@gmail.com>
2025-10-13 19:13:30 +0600

    dashboard layout changed

--- src/components/admin/DashboardLayout.jsx
+++ src/components/admin/DashboardLayout.jsx
@@ -1,9 +1,11 @@
+import { SmilePlusIcon } from 'lucide-react';
 import React from 'react';
 
 const DashboardLayout = () => {
     return (
         <div>
-            <h1 className='items-center justify-center flex'>Hello Admin, Welcome to your universe!!</h1>
+            <h1 className='items-center justify-center flex font-semibold'>Hello Admin, Welcome to Your Universe!!</h1>
+            <span className='justify-center items-center flex mt-4'> <SmilePlusIcon></SmilePlusIcon></span>      
         </div>
     );
 };
